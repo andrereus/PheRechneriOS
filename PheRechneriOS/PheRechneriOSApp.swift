437b2bc8ac6f7062a6dba55850f7890f5cbc34df
Andre Reus <andrereus@users.noreply.github.com>
2023-03-24 21:28:38 +0100

    Initial Commit

---
+++ PheRechneriOS/PheRechneriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PheRechneriOSApp.swift
+//  PheRechneriOS
+//
+//  Created by André Reus on 24.03.23.
+//
+
+import SwiftUI
+
+@main
+struct PheRechneriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
